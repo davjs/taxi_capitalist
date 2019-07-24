9e5fc3b89a4658d588f28fe00010c093a42fe275
augustalex <augustalexandersson@gmail.com>
2019-07-24 15:35:45 +0200

    New spawn system

---
+++ Assets/PickupAndDropoff/Passenger.cs
@@ -0,0 +1,25 @@
+﻿using Taxi;
+using UnityEngine;
+
+namespace PickupAndDropoff
+{
+    public class Passenger : MonoBehaviour
+    {
+        public Vector3 OriginalSpawnPoint;
+        public PassengerSpawner Spawner;
+
+        private void OnCollisionEnter(Collision other)
+        {
+            var otherPlayer = other.gameObject.GetComponent<Player>();
+            if (otherPlayer)
+            {
+                Spawner.RegisterPassengerPickedUpAtPointByPlayer(OriginalSpawnPoint, otherPlayer);
+
+                var personHolder = otherPlayer.GetComponent<TaxiPersonHolder>();
+                personHolder.LoadPersons();
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
