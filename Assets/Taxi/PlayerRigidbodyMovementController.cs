4f5e5b8a3bfa9a65d9f1b17b6bcd41ec3ea2ff1f
augustalex <augustalexandersson@gmail.com>
2019-07-24 18:19:20 +0200

    Added speed limit

--- Assets/Taxi/PlayerRigidbodyMovementController.cs
+++ Assets/Taxi/PlayerRigidbodyMovementController.cs
@@ -1,8 +1,11 @@
 using System;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
-namespace Taxi {
-    public class PlayerRigidbodyMovementController : MonoBehaviour {
+namespace Taxi
+{
+    public class PlayerRigidbodyMovementController : MonoBehaviour
+    {
         public float velocity = 15;
         public float rotationSpeed = 180;
         public ParticleSystem speedyParticles;
@@ -11,17 +14,22 @@ namespace Taxi {
         private PlayerInputController inputController;
         private new Rigidbody rigidbody;
 
-        private void Start() {
+        private void Start()
+        {
             inputController = GetComponent<PlayerInputController>();
             rigidbody = GetComponent<Rigidbody>();
         }
 
-        void Update() {
+        void Update()
+        {
             rigidbody.useGravity = transform.position.y > 0.6f;
-            if (rigidbody.velocity.magnitude > 50) {
+            if (rigidbody.velocity.magnitude > 50)
+            {
                 speedyParticles.Emit(200);
             }
-            switch (movementType) {
+
+            switch (movementType)
+            {
                 case PlayerMovementType.ManualRotation:
                     UpdateMoveManualRotation();
                     return;
@@ -38,20 +46,27 @@ namespace Taxi {
             }
         }
 
-        private void UpdateMoveSlide() {
-            //if (this.rigidbody.velocity.magnitude >= 10) return;
-
+        private void UpdateMoveSlide()
+        {
             this.rigidbody.AddForce(inputController.horizontal * Vector3.right * velocity * Time.deltaTime);
             this.rigidbody.AddForce(inputController.vertical * Vector3.forward * velocity * Time.deltaTime);
+
+            if (this.rigidbody.velocity.magnitude >= 30)
+            {
+                rigidbody.velocity = new Vector3(Math.Min(rigidbody.velocity.x, 30), rigidbody.velocity.y,
+                    Math.Min(rigidbody.velocity.z, 30));
+            }
         }
 
-        private void UpdateMoveManualRotation() {
+        private void UpdateMoveManualRotation()
+        {
             this.rigidbody.AddForce(inputController.vertical * this.transform.forward * velocity * Time.deltaTime);
             var rotationAmount = rotationSpeed * Time.deltaTime * inputController.horizontal;
             this.rigidbody.AddTorque(0, rotationAmount, 0);
         }
 
-        private void UpdateMoveAdaptiveRotation() {
+        private void UpdateMoveAdaptiveRotation()
+        {
             var inputDirection = new Vector3(inputController.horizontal, 0, inputController.vertical);
             var thrust = Vector3.Dot(inputDirection.normalized, this.transform.forward);
             var rotation =
