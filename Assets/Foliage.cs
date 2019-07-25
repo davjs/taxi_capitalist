631523d8fb4ec8ae003bea6643576a3a013f9815
augustalex <augustalexandersson@gmail.com>
2019-07-25 12:27:26 +0200

    Foliage now slows you down

--- Assets/Foliage.cs
+++ Assets/Foliage.cs
@@ -7,12 +7,19 @@ public class Foliage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var rigidbody = other.gameObject.GetComponentInParent<Rigidbody>();
+        if (rigidbody && rigidbody.velocity.magnitude > 10)
+        {
+            rigidbody.velocity = rigidbody.velocity * .01f;
+        }
     }
 }
