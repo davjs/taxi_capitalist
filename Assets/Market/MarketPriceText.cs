43bb00aa2731225de63e0cce781426ba2b63d34f
Anton <a.e.lindkvist@gmail.com>
2019-07-25 18:01:04 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/Market/MarketPriceText.cs
+++ Assets/Market/MarketPriceText.cs
@@ -2,22 +2,23 @@
 using Taxi;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MarketPriceText : MonoBehaviour
 {
-    private Text _text;
+    private TextMeshPro _text;
     private Market.Market _market;
 
     void Start()
     {
-        _text = GetComponent<Text>();
+        _text = GetComponentInChildren<TextMeshPro>();
 
         _market = GameObject.FindGameObjectWithTag("Market").GetComponent<Market.Market>();
     }
 
     void Update()
     {
-        _text.text = _market.StockQt + "% stake for $" + _market.StockPrice;
+        _text.text = _market.StockQt + "% Share \n$" + _market.StockPrice;
     }
 
     private Player FindPlayerWithId(string id)
