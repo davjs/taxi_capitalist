80c360be653202ab2a2a2271b8d9b7b5efd12bc9
augustalex <augustalexandersson@gmail.com>
2019-07-25 18:30:07 +0200

    More expensive upgrades

--- Assets/UpgradeBuilding.cs
+++ Assets/UpgradeBuilding.cs
@@ -1,34 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Taxi;
 using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public enum Upgrade {
+public enum Upgrade
+{
     Speed,
     Size
 }
 
-public class UpgradeBuilding : MonoBehaviour {
-    public int Cost = 50;
-    public float SpeedIncrease = 500f;
+public class UpgradeBuilding : MonoBehaviour
+{
+    [NonSerialized] public int Cost = 1000;
+    [NonSerialized] public float SpeedIncrease = 500f;
     public Upgrade? CurrentUpgrade = Upgrade.Speed;
     public GameObject SizeIcon;
     public GameObject SpeedIcon;
     public TextMeshPro PriceTag;
 
-    private void Start() {
+    private void Start()
+    {
         ClearUpgrade();
         Invoke("GetNewUpgrade", 5f);
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
         var player = other.gameObject.GetComponentInParent<Player>();
         var taxiController = other.gameObject.GetComponentInParent<PlayerRigidbodyMovementController>();
         var personHolder = other.gameObject.GetComponentInParent<TaxiPersonHolder>();
         if (!taxiController || !personHolder || player.Money < Cost || CurrentUpgrade == null) return;
 
-        if (CurrentUpgrade == Upgrade.Speed && !player.SpeedUpgrade) {
+        if (CurrentUpgrade == Upgrade.Speed && !player.SpeedUpgrade)
+        {
             taxiController.velocity += SpeedIncrease;
             player.Money -= Cost;
             player.SpeedUpgrade = true;
@@ -36,7 +43,8 @@ public class UpgradeBuilding : MonoBehaviour {
             Invoke("GetNewUpgrade", 0.5f);
         }
 
-        if (CurrentUpgrade == Upgrade.Size) {
+        if (CurrentUpgrade == Upgrade.Size)
+        {
             personHolder.Capacity += 1;
             player.Money -= Cost;
             ClearUpgrade();
@@ -44,21 +52,26 @@ public class UpgradeBuilding : MonoBehaviour {
         }
     }
 
-    private void ClearUpgrade() {
+    private void ClearUpgrade()
+    {
         CurrentUpgrade = null;
         PriceTag.text = "";
         SizeIcon.SetActive(false);
         SpeedIcon.SetActive(false);
     }
 
-    private void GetNewUpgrade() {
+    private void GetNewUpgrade()
+    {
         CurrentUpgrade = (Upgrade) Random.Range(0, 2);
-        PriceTag.text = Cost + "$";
-        if (CurrentUpgrade == Upgrade.Size) {
+        PriceTag.text = "$" + Cost;
+        if (CurrentUpgrade == Upgrade.Size)
+        {
             SizeIcon.SetActive(true);
         }
-        if (CurrentUpgrade == Upgrade.Speed) {
-            SpeedIcon.SetActive(true);            
+
+        if (CurrentUpgrade == Upgrade.Speed)
+        {
+            SpeedIcon.SetActive(true);
         }
     }
 }
