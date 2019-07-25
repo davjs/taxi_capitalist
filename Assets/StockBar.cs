985f209c730d592103fbe7551259fec889458c64
David Jensen <david_jensen3@hotmail.com>
2019-07-25 14:32:33 +0200

    Stockbars wip

---
+++ Assets/StockBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Taxi;
+using UnityEngine;
+
+public class StockBar : MonoBehaviour {
+    public int PlayerIndex;
+    public float MaxHeight = 5.0f;
+    public float StocksGoal = 20;
+
+    private Player _player;
+
+    private void Start() {
+        _player = GameObject.FindGameObjectsWithTag("Player")
+            .ElementAt(PlayerIndex)
+            .GetComponent<Player>();
+        var mesh = GetComponentInChildren<MeshRenderer>();
+        mesh.material.color = _player.GetColor();
+    }
+
+    private void Update() {
+        var height = (_player.Stocks / StocksGoal) * MaxHeight;
+        transform.localScale = new Vector3(1, 1, height);
+    }
+
+}
