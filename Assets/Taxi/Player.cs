79e68ca7898574277f781b8979e0ac4868b99045
David Jensen <david_jensen3@hotmail.com>
2019-07-25 19:56:23 +0200

    CollisionMoney

--- Assets/Taxi/Player.cs
+++ Assets/Taxi/Player.cs
@@ -1,9 +1,7 @@
 ﻿using UnityEngine;
 
-namespace Taxi
-{
-    public class Player : MonoBehaviour
-    {
+namespace Taxi {
+    public class Player : MonoBehaviour {
         public int Money = 0;
         public int Stocks = 0;
         public bool SpeedUpgrade = false;
@@ -12,88 +10,80 @@ namespace Taxi
 
         private Rigidbody rb;
 
-        private void Start()
-        {
+        private void Start() {
             rb = GetComponent<Rigidbody>();
         }
 
-        public void EarnMoney(int money)
-        {
+        public void EarnMoney(int money) {
             Money += money;
             TextMeshManager.InstantiateText(transform.position, "+" + money + "$", Id);
         }
 
-        public Color GetColor()
-        {
-            if (Id.Equals("1"))
-            {
-                return  new Color(1, 86 / 255f, 86 / 255f);
+        public Color GetColor() {
+            if (Id.Equals("1")) {
+                return new Color(1, 86 / 255f, 86 / 255f);
             }
 
-            if (Id.Equals("2"))
-            {
+            if (Id.Equals("2")) {
                 return new Color(86 / 255f, 1, 1);
             }
 
-            if (Id.Equals("3"))
-            {
+            if (Id.Equals("3")) {
                 return new Color(1, 86 / 255f, 1);
             }
 
-            if (Id.Equals("4"))
-            {
+            if (Id.Equals("4")) {
                 return Color.yellow;
             }
 
             return Color.black;
         }
 
-        public void AddStock(int stocks)
-        {
+        public void AddStock(int stocks) {
             Stocks += stocks;
         }
 
-        public void SpendMoney(int amount)
-        {
+        public void SpendMoney(int amount) {
             Money -= amount;
         }
 
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (collision.gameObject.tag == "Player")
-            {
+        private void OnCollisionEnter(Collision collision) {
+            if (collision.gameObject.tag == "Player") {
+                var player1Speed = rb.velocity.magnitude;
+                var player2Speed = collision.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude;
+                if (player1Speed > player2Speed) {
+                    EarnMoney(1);
+                }
+                else {
+                    collision.gameObject.GetComponent<Player>().EarnMoney(1);
+                }
+
                 rb.AddExplosionForce(collisionForce, collision.GetContact(0).point, 2f);
             }
         }
 
-        public string GetColorName()
-        {
-            if (Id.Equals("1"))
-            {
+        public string GetColorName() {
+            if (Id.Equals("1")) {
                 return "Red";
             }
 
-            if (Id.Equals("2"))
-            {
+            if (Id.Equals("2")) {
                 return "Cyan";
             }
 
-            if (Id.Equals("3"))
-            {
+            if (Id.Equals("3")) {
                 return "Purple";
             }
 
-            if (Id.Equals("4"))
-            {
+            if (Id.Equals("4")) {
                 return "Yellow";
             }
 
             return "Black";
         }
 
-        public int CalculateInterest(int value)
-        {
-            return (int)((Stocks / 100.0f) * value);
+        public int CalculateInterest(int value) {
+            return (int) ((Stocks / 100.0f) * value);
         }
     }
 }
