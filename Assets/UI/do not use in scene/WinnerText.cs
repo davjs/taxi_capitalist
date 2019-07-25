e8e880166039e6f5c1e5c472cbd69429eaa3a3f2
augustalex <augustalexandersson@gmail.com>
2019-07-25 13:54:02 +0200

    You can now win the game

---
+++ Assets/UI/do not use in scene/WinnerText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WinnerText : MonoBehaviour
+{
+    private Text _text;
+    private Market.Market _market;
+
+    void Start()
+    {
+        _text = GetComponent<Text>();
+        _market = GameObject.FindGameObjectWithTag("Market").GetComponent<Market.Market>();
+    }
+
+    void Update()
+    {
+        if (_market.GameHasEnded())
+        {
+            var number = int.Parse(_market.GetLeadingPlayerId());
+            _text.text = "Player " + number + " has won";
+        }
+    }
+}
