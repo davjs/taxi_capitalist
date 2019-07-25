cf7997586254100f8fd36836da9963ab66e70b3e
David Jensen <david_jensen3@hotmail.com>
2019-07-25 13:56:32 +0200

    ...

--- Assets/PickupAndDropoff/Passenger.cs
+++ Assets/PickupAndDropoff/Passenger.cs
@@ -1,25 +1,31 @@
 ﻿using Taxi;
 using UnityEngine;
 
-namespace PickupAndDropoff
-{
-    public class Passenger : MonoBehaviour
-    {
+namespace PickupAndDropoff {
+    public class Passenger : MonoBehaviour {
         public Vector3 OriginalSpawnPoint;
         public PassengerSpawner Spawner;
 
-        private void OnCollisionEnter(Collision other)
-        {
+        private void OnTriggerEnter(Collider other) {
             var otherPlayer = other.gameObject.GetComponent<Player>();
             if (!otherPlayer) return;
 
             var personHolder = otherPlayer.GetComponent<TaxiPersonHolder>();
-            if (!personHolder.CanFitMore()) return;
+            if (!personHolder.CanFitMore()) {
+                transform.Rotate(Vector3.right, 90);
+                Invoke("DestroySelf", 5.0f);
+                GetComponent<Collider>().enabled = false;
+                return;
+            }
 
             Spawner.RegisterPassengerPickedUpAtPointByPlayer(OriginalSpawnPoint, otherPlayer);
 
             personHolder.LoadPersons();
 
+            DestroySelf();
+        }
+
+        private void DestroySelf() {
             Destroy(gameObject);
         }
     }
