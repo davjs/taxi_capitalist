b5d30c576b46d5a54e43f3e5c425f057519275dc
Anton <a.e.lindkvist@gmail.com>
2019-07-25 11:28:12 +0200

    Changed barrel script to return to its spawnpoint after a timer

--- Assets/scenery/barrel.cs
+++ Assets/scenery/barrel.cs
@@ -6,11 +6,25 @@ public class barrel : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private float explosiveForce = 100.0f;
+    public float resetTimerLength = 10.0f;
+    public GameObject lid;
+    private Rigidbody lidRb;
 
+    private Vector3 startPos;
+    private Quaternion startRot;
+    private Vector3 lidStartPos;
+    private Quaternion lidStartRot;
 
     void Start()
     {
+        startPos = transform.position;
+        startRot = transform.rotation;
+        lidStartPos = lid.transform.position;
+        lidStartRot = lid.transform.rotation;
+
         rb = GetComponent<Rigidbody>();
+        lidRb = lid.GetComponent<Rigidbody>();
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -19,7 +33,22 @@ public class barrel : MonoBehaviour
         {
             rb.useGravity = true;
             rb.AddExplosionForce(explosiveForce, transform.position, 1.0f);
-        }
+            lidRb.useGravity = true;
+            lidRb.AddExplosionForce(explosiveForce / 2, lid.transform.position, 1.0f);
 
+            StartCoroutine(ResetAfterTime(resetTimerLength));
+        }
+    }
+
+    private IEnumerator ResetAfterTime(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        transform.SetPositionAndRotation(startPos, startRot);
+        rb.isKinematic = true;
+        rb.isKinematic = false;
+
+        lid.transform.SetPositionAndRotation(lidStartPos, lidStartRot);
+        lidRb.isKinematic = true;
+        lidRb.isKinematic = false;
     }
 }
